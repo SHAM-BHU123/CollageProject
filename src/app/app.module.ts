20bc840061ed482e370c04866ef6588cb886dffa
shambhu pandey <sorajpandey666@gmail.com>
2023-02-15 20:20:25 +0545

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
 
+import { LoginComponent } from './shared/login/login.component';
+import { SignupComponent } from './shared/signup/signup.component';
+import { RegisterComponent } from './shared/register/register.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'register', component: RegisterComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutusComponent,
+    ServicesComponent,
+    LoginComponent,
+    SignupComponent,
+    LoginComponent,
+    SignupComponent,
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
